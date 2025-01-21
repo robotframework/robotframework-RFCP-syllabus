075995500df05c213afa6d8c029fdbcb73520104
Many Kasiriha <many.kasiriha@dbschenker.com>
2025-01-21 16:24:33 +0100

    Add docusaurus-lunr-search

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -159,6 +159,7 @@ const config = {
         additionalLanguages: ['robotframework', 'rust'],
       },
     }),
+  plugins: [require.resolve('docusaurus-lunr-search')],
 };
 
 module.exports = config;
