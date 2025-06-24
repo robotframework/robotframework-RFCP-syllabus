acb6886f05000487c886e7e99d7427433fb0c22a
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-06-24 12:14:59 +0200

    Result storage (#10)
    * Added zustand as a central store and storing all quizzes in it. Now the property name is required that needs to be unique on each page.
    
    * Updated quizdown-extended to 0.6.5
    
    * Added storage for quiz results
    
    * Saving quizresults in localstorage
    
    * Added quizResultpage-sidebar
    
    * Made first version of the quiz result page
    
    * Fixed quiz prebuild script
    
    * Next version of the resultsPage
    
    * Added link to view the results to the quiz component
    
    * Fixed build error
    
    * Fixed resultpage

--- website/src/components/Quiz/Quiz.tsx
+++ website/src/components/Quiz/Quiz.tsx
@@ -1,10 +1,23 @@
+// @ts-ignore
+// eslint-disable @typescript-eslint/eslint/no-var-requires
+
 import React from 'react';
 import { useEffect } from 'react';
 import 'quizdown-extended'
-import hljsDefineRobot from '../robot';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+import useQuizStore from './QuizStore';
+import hljsDefineRobot from '../../robot';
 import 'quizdown-extended/public/build/extensions/quizdownHighlight';
+import './Quiz.css'
+
 
 export default function Quiz(props) {
+  
+  const quiz = useQuizStore((state) => state.quiz);
+  const addQuizResult = useQuizStore((state) => state.addQuizResult);
+  const resultBaseUrl = useBaseUrl('/quizResults');
 
   const startQuiz = () => {
     let node = document.querySelector('#quizDownContainer');
@@ -12,22 +25,29 @@ export default function Quiz(props) {
     const config = {
       startOnLoad: true,          // detect and convert all div html elements with class quizdown
       shuffleAnswers: true,       // shuffle answers for each question
-      shuffleQuestions: false,    // shuffle questions for each quiz
-      nQuestions: undefined,       // display n questions at random, if shuffleQuestions is true
+      shuffleQuestions: true,     // shuffle questions for each quiz
+      nQuestions: undefined,      // display n questions at random, if shuffleQuestions is true
       primaryColor: 'var(--quizdownPrimaryColor)',  // primary CSS color
       secondaryColor: 'var(--quizdownSecondaryColor)',  // secondary CSS color
       textColor: 'var(--quizdownTextColor)',         // text color of some elements
-      buttonColor: 'var(--quizdownSecondaryColor)',          // The background color of the buttons
+      buttonColor: 'var(--quizdownSecondaryColor)',  // The background color of the buttons
       locale: null,               // language of the user interface (auto-detect per default)
       enableRetry: true           // allow the user to resubmit answers
     };
 
 
     quizdownHighlight.registerHljsLanguage("robot", hljsDefineRobot);
-    quizdown.register(quizdownHighlight).createApp(props.question, node, config);
+    let quizDown = quizdown.register(quizdownHighlight)
+    quizDown.createApp(props.question, node, config);
+
+    quizDown.listenForStats(node, (event) => {
+      addQuizResult(generateQuizId(props.name), event.numberOfQuestions, event.solved, event.right, event.wrong);
+    });
+
+    return quizDown;
   };
-  
-  
+
+
   const setColorsDependingOnTheme = () => {
     const theme = document.documentElement.getAttribute('data-theme');
     if (theme === 'light') {
@@ -41,7 +61,26 @@ export default function Quiz(props) {
     }
   }
 
+  const generateQuizId = (name: string) => {
+
+    let page = window.location.pathname;
+    page = page.replace("/robotframework-RFCP-syllabus/", ""); // Remove beginning of the url
+
+    let id = page.replace("docs/", "") + "#" + name.replace(" ", "+");
+    id = id.toLocaleLowerCase();
+
+    return id;
+  }
+
+  const generateResultLink = () => {
+    if (ExecutionEnvironment.canUseDOM) {
+    return resultBaseUrl + "#" + generateQuizId(props.name);
+    } 
+    return "#";
+  }
+
   useEffect(() => {
+    // Add quiz to the central storage
 
     // To set the color scheme of quizdown
     document.documentElement.style.setProperty('--quizdownPrimaryColor', "var(--ifm-color-primary)");
@@ -54,12 +93,16 @@ export default function Quiz(props) {
 
     startQuiz();
     setColorsDependingOnTheme();
-    return () => observer.disconnect();
-
-  });
+    return () => {
+      observer.disconnect();
+    };
+  }, [props.question]); // Add dependency array with props.question
 
   return (
-    < span id="quizDownContainer" className='quizdown'>
-    </span >
+    <span id="quizContainer">
+      <span id="quizDownContainer" className='quizdown'>
+      </span>
+      <a id="resultLink" href={ generateResultLink() } >View results</a>
+    </span>
   );
 }
